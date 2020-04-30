3da6be461c1291c91dc8743c88b76b194e2614e3
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-30 12:11:56 +0300

    Setup the view engine, static files and body parser

--- config/express.js
+++ config/express.js
@@ -1,26 +1,17 @@
 const express = require('express');
-// const handlebars = require('express-handlebars');
-// const bodyParser = require('body-parser');
+const handlebars = require('express-handlebars');
+const path = require('path');
+const bodyParser = require('body-parser');
 
 const port = 4200;
 
 module.exports = (app) => {
-    
+    app.use(bodyParser.urlencoded( { extended: false } ));
+    app.use(express.static(path.resolve(__basedir, 'static')));
+    app.engine('.hbs', handlebars({ extname: '.hbs'}));
+    app.set('views', path.resolve(__basedir, 'views'));
+
     //TODO: Setup the view engine
-    app = express();
-
-    app.get('/', (req, res) => {
-        res.status(200);
-        res.send('Created an express app.');
-    });
-
-    app.listen(port, () => {
-        console.log(`Express running on port ${port}...`);
-    });
-
-
-    //TODO: Setup the body parser
-
     //TODO: Setup the static files
-
+    //TODO: Setup the body parser
 };
