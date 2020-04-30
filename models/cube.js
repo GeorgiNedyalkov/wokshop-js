12af5c323e2e127dea3fbfa3d4f43701c95fc12f
Georgi Nedyalkov <gnedyalkov94@gmail.com>
2020-04-30 12:38:43 +0300

    Implemented an update method for the cube model

--- models/cube.js
+++ models/cube.js
@@ -5,6 +5,16 @@ class Cube {
         this.data = require('../config/database.json');
     }
 
+    _write(newData, resolveData) {
+        return new Promise((resolve, reject) => {
+            fs.writeFile(path.resolve('../config/data.json'), JSON.stringify(newData), (err) => {
+                if (err) { reject(err); return; }
+                this.data = newData;
+                resolve(resolveData);
+            });
+        });
+    }
+
     insert(newCube) {
         const newIndex = this.data.lastIndex++;
         newCube = { id: newIndex, ...newCube };
@@ -12,20 +22,27 @@ class Cube {
             lastIndex: newIndex,
             entities: this.data.entities.concat(newCube)
         };
-        return new Promise((resolve, reject) => {
-            fs.writeFile(path.resolve('../config/data.json'), JSON.stringify(newData), (err) => {
-                if (err) { reject(err); return; }
-                this.data = newData;
-                resolve(newCube);
-            });
-        });
+
+        return this._write(newData, newCube);
+        
+    }
+
+    update(cubeId, updates) {
+        const entitityIndex = this.data.entities.findIndex(({ id }) => id === cubeId);
+        const entity = this.data.entities[entitityIndex];
+        const updatedEntity = {...entitity, ...updates};
+        const newData = {
+            lastIndex: this.data.lastIndex,
+            entities: [
+                ...this.data.entities.slice(0, entitityIndex),
+                updatedEntity,
+                ...this.data.entities.slice(entitityIndex + 1)
+            ]
+        };
+        return this._write(newData, updatedEntity);
     }
 
     delete() {
-
-    }
-
-    update() {
 
     }
 
